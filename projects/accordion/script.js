6d689fa79b219defc1375b154e3fd600397e17f4
iamsaief <saiefalemon@gmail.com>
2020-11-28 22:01:54 +0600

    modified - accordion transitions

--- projects/accordion/script.js
+++ projects/accordion/script.js
@@ -2,11 +2,30 @@
 const panelItems = document.querySelectorAll(".accordion-panel-item");
 
 panelItems.forEach((currentItem) => {
+	const headerHeight = currentItem.querySelector(".accordion-panel-header").offsetHeight;
+	const contentHeight = currentItem.querySelector(".accordion-panel-content").offsetHeight;
+
+	if (currentItem.classList.contains("active")) {
+		currentItem.style.height = `${currentItem.offsetHeight}px`;
+	} else {
+		currentItem.style.height = `${headerHeight}px`;
+	}
+
+	// adding .active and updating heights
 	currentItem.addEventListener("click", () => {
-		currentItem.classList.toggle("active");
+		if (!currentItem.classList.contains("active")) {
+			currentItem.classList.add("active");
+			currentItem.style.height = `${headerHeight + contentHeight}px`;
+		} else {
+			currentItem.classList.remove("active");
+			currentItem.style.height = `${headerHeight}px`;
+		}
+
+		// removing .active from item that hasn't clicked
 		panelItems.forEach((item) => {
 			if (item !== currentItem) {
 				item.classList.remove("active");
+				item.style.height = `${headerHeight}px`;
 			}
 		});
 	});
